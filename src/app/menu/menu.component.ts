ccf3a0ea5f5a9c3263c338f0240bff00d5674cac
Sharma <vivek.4.sharma@on.nokia.com>
2017-09-19 00:43:40 +0530

    fixed issues in pricing, added nutrition icons

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -167,16 +167,17 @@ export class MenuComponent implements OnInit {
             }
           }
 
-          total =  parseInt(this.items[i].originalItemCost)*this.items[i].Product.qty;
+          let getOICost = Number(parseFloat(this.items[i].originalItemCost).toFixed(2));
+          total =  getOICost*this.items[i].Product.qty;
           this.items[i].totalItemCost = total;
 
           break;
       }
     }
 
-    this.totalCost =  this.utilService.calculateOverAllCost(this.items);
+    let getTCost = Number(this.utilService.calculateOverAllCost(this.items).toFixed(2));
+    this.totalCost =  getTCost;
     this.netCost = this.totalCost;
-   // console.log(type, this.totalCost, this.items.Product.qty);
   }
 
 
